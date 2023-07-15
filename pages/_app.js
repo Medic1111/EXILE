713cbd4590370651491c33c5ff56310b6b851da7
Aryse Tansy <medictansy@gmail.com>
2023-07-15 08:35:33 -0400

    SEO Optimization

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,30 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Exile Tattoo and Piercing, LLC</title>
+        <meta
+          name="description"
+          content="Exile Tattoo and Piercing, LLC. Miami Beach local tattoo and body piercing shop located at: 6634 Collins Avenue, Miami Beach, FL 33141, USA"
+        />
+        <meta
+          property="og:title"
+          content="Exile Tattoo and Piercing, LLC"
+          key="ogtitle"
+        />
+        <meta
+          property="og:description"
+          content="Exile Tattoo and Piercing, LLC. Miami Beach local tattoo and body piercing shop located at: 6634 Collins Avenue, Miami Beach, FL 33141, USA"
+          key="ogdesc"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
