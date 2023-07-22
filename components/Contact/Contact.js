cd8c696631fb52995e8c7f5c18ecff85c512b63b
Aryse Tansy <medictansy@gmail.com>
2023-07-22 16:57:18 -0400

    Minor adjusts

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -1,8 +1,8 @@
 import Text from "./Text/Text";
-
 import ContactForm from "./ContactForm/ContactForm";
 import Hours from "./Hours/Hours";
 import { styles } from "./styles";
+import Map from "./Map/Map";
 
 const Contact = () => {
   return (
@@ -12,6 +12,16 @@ const Contact = () => {
         <Hours />
       </div> */}
       <ContactForm />
+      <p
+        className={`${styles.formTitle}  p-5 hover:text-rose-600 cursor-pointer`}
+      >
+        .786
+        <br />
+        .507
+        <br />
+        .5653
+      </p>
+      <Map />
       {/* <Text />
       <Hours /> */}
     </section>
--- components/Profile/styles.js
+++ components/Profile/styles.js
@@ -1,14 +1,14 @@
 export const styles = {
-  li: "flex flex-col lg:flex-row justify-between bg-slate-50 h-screen  w-screen  min-h-[85vh] border-solid border-b-4 mt-[-5px] border-[#0d1322] ",
+  li: "flex flex-col lg:flex-row justify-between bg-slate-50 min-h-screen  w-screen pb-10  border-solid border-b-4 mt-[-5px] border-[#0d1322] ",
   mainContainer: "h-[100%] flex  flex-col justify-between w-full   ",
-  infoBox: " border-t-4 border-solid border-[#0d1322]",
+  infoBox: " border-t-4 border-solid border-[#0d1322] ",
   infoP:
-    "h-[7.5vh] lg:h-[10vh] text-[#0d1322] flex items-center justify-start text-5xl border-b-2 pl-1 lg:text-6xl   border-solid border-[#0d1322]",
+    "h-24  sm:h-[7.5vh] lg:h-[10vh] text-[#0d1322] flex items-center justify-start text-5xl  pl-1 lg:text-6xl font-light  border-solid border-[#0d1322]",
   redSpan: "text-rose-700",
   btnImgBoxOutter: "mb-10 h-full  flex items-start gap-5 justify-end flex-col ",
   btnImgBox:
     "w-full flex h-full flex-col items-end border-t-2 border-solid border-[#0d1322]",
-  btn: "flex items-center justify-center text-xl lg:text-2xl hover:bg-rose-600   cursor-pointer w-2/3  border-l-2  h-[7.5vh] border-solid border-[#0d1322] text-[#0d1322]",
+  btn: "flex items-center font-light justify-center text-xl lg:text-2xl hover:bg-rose-600   cursor-pointer w-2/3  border-l-2 h-24  sm:h-[7.5vh] border-solid border-[#0d1322] text-[#0d1322]",
   imgBox:
     "lg:border-l-2 border-b-2  border-l-2 lg:border-t-0 lg:border-r-0 border-solid border-[#0d1322] h-[55%] w-[50%]   flex justify-end items-end",
   img: "h-full  object-cover",
