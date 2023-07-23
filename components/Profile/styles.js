866d86a505088b41b5e7e67d7248190b55bd0a0d
Aryse Tansy <medictansy@gmail.com>
2023-07-23 07:50:24 -0400

    Minor: BTN PROFILE MOBILE HEIGHT

--- components/Profile/styles.js
+++ components/Profile/styles.js
@@ -8,7 +8,7 @@ export const styles = {
   btnImgBoxOutter: "mb-10 h-full  flex items-start gap-5 justify-end flex-col ",
   btnImgBox:
     "w-full flex h-full flex-col items-end border-t-2 border-solid border-[#0d1322]",
-  btn: "font-san flex items-center font-light justify-center text-xl lg:text-2xl hover:bg-rose-600   cursor-pointer w-2/3  border-l-2 h-24  sm:h-[7.5vh] border-solid border-[#0d1322] text-[#0d1322]",
+  btn: "font-san flex items-center font-light justify-center text-xl lg:text-2xl hover:bg-rose-600   cursor-pointer w-2/3  border-l-2 h-16  sm:h-[7.5vh] border-solid border-[#0d1322] text-[#0d1322]",
   imgBox:
     "lg:border-l-2 border-b-2  border-l-2 lg:border-t-0 lg:border-r-0 border-solid border-[#0d1322] h-[55%] w-[50%]   flex justify-end items-end",
   img: "h-full  object-cover",
