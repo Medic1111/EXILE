77f6f192301567183dff447faa958a103cd36744
Aryse Tansy <medictansy@gmail.com>
2023-07-23 07:35:10 -0400

    Hero mobile fix

--- components/Hero/styles.js
+++ components/Hero/styles.js
@@ -1,9 +1,9 @@
 export const styles = {
   section:
-    "font-marker h-screen overflow-auto bg-slate-50 w-full flex flex-col  start-center justify-between   px-5 py-5 w-full",
+    "font-marker h-screen overflow-auto bg-slate-50 w-full flex flex-col start-center  sm:start-center justify-center sm:justify-between gap-[15vh] sm:gap-0  px-5 py-5 w-full",
   txtBox: "w-full",
   title:
-    "font-normal uppercase  text-[#0d1322] text-6xl  sm:text-8xl sm:max-w-[50%] lg:max-w-[50%] sm:text-8xl  xl:text-[9rem]  w-full ",
+    "font-normal uppercase  text-[#0d1322] text-6xl text-[4em]  sm:text-8xl sm:max-w-[50%] lg:max-w-[50%] sm:text-8xl  xl:text-[9rem]  w-full ",
   img: "border-solid border-2 border-[#0d1322] w-full  object-cover sm:h-[50vh] lg:h-[60vh]",
   btn: "h-20 min-h-16 cursor-pointer flex items-center justify-end px-5 sm:px-10 text-5xl border-2 border-solid border-[#0d1322] text-[#0d1322]  bg-rose-600 hover:bg-slate-50   mb-20 md:mb-0 ",
 };
