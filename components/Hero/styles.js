3664884f16dfabf8239649103a37c5faa4ae09c4
Aryse Tansy <medictansy@gmail.com>
2023-07-23 10:06:43 -0400

    border cta

--- components/Hero/styles.js
+++ components/Hero/styles.js
@@ -4,5 +4,5 @@ export const styles = {
   txtBox: "w-full",
   title:
     "font-normal uppercase  text-[#0d1322] text-6xl text-[4em]  sm:text-8xl sm:max-w-[50%] lg:max-w-[50%] sm:text-8xl  xl:text-[9rem]  w-full ",
-  btn: "h-20 min-h-16 cursor-pointer flex items-center justify-end px-5 sm:px-10 text-5xl border-2 border-solid border-[#0d1322] text-[#0d1322]  bg-rose-600 hover:bg-slate-50   mb-20 md:mb-0 ",
+  btn: "h-20 min-h-16 cursor-pointer flex items-center justify-end px-5 sm:px-10 text-5xl border-4 border-solid border-[#0d1322] text-[#0d1322]  bg-rose-600 hover:bg-slate-50   mb-20 md:mb-0 ",
 };
