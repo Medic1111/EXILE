c3285397daf4f91360cc729ba5b65e7a1a593c88
Aryse Tansy <medictansy@gmail.com>
2023-07-29 15:31:57 -0400

    Clean up

--- components/Contact/Phone/Phone.jsx
+++ components/Contact/Phone/Phone.jsx
@@ -6,7 +6,7 @@ const Phone = () => {
       <a
         href="tel:786507-5653"
         aria-label="7 8 6. 5 0 7. 5 6 5 3."
-        className={`${styles.formTitle} font-marker  cursor-pointer pl-5 text-start hover:underline decoration-rose-600 transition  duration-2000 ease-in-out text-8xl  sm:text-7xl md:text-9xl xl:text-9xl`}
+        className={`${styles.formTitle} font-marker  cursor-pointer pl-5 text-start hover:underline decoration-rose-600 transition  duration-2000 ease-in-out text-8xl  sm:text-7xl md:text-9xl  xl:text-[14rem]`}
       >
         .786
         <br />
--- components/Hero/styles.js
+++ components/Hero/styles.js
@@ -3,6 +3,6 @@ export const styles = {
     "font-marker h-screen  bg-slate-50 w-full flex flex-col start-center  sm:start-center justify-center sm:justify-between gap-[15vh] sm:gap-0  px-5 py-5 w-full",
   txtBox: "w-full",
   title:
-    "font-normal uppercase  text-[#0d1322] text-6xl text-[4em]  sm:text-8xl sm:max-w-[50%] lg:max-w-[50%] sm:text-8xl  xl:text-[9rem]  w-full ",
+    "font-normal uppercase  text-[#0d1322] text-6xl  text-[4em]  sm:text-8xl sm:max-w-[50%] lg:max-w-[50%] sm:text-8xl  xl:text-[9rem]  w-full ",
   btn: "h-20 min-h-16 cursor-pointer flex items-center justify-end px-5 sm:px-10 text-5xl border-4 border-solid border-[#0d1322] text-[#0d1322]  bg-rose-600 hover:bg-slate-50   mb-20 md:mb-0 ",
 };
