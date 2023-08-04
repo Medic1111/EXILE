bd1ec4645429677cb79e73a42766a017ae38490b
Aryse Tansy <medictansy@gmail.com>
2023-08-04 13:57:26 -0400

    Abstraction

---
+++ components/Shop/data/data.js
@@ -0,0 +1,14 @@
+export const urlArr = [
+  {
+    url: "https://res.cloudinary.com/dfid6vhes/video/upload/v1690569093/tattoing_ul83ys.mp4",
+  },
+  {
+    url: "https://res.cloudinary.com/dfid6vhes/video/upload/v1690572772/replace_mcnn6c.mp4",
+  },
+  {
+    url: "https://res.cloudinary.com/dfid6vhes/video/upload/v1690572295/tattoing2_g2ubhq.mp4",
+  },
+  {
+    url: "https://res.cloudinary.com/dfid6vhes/video/upload/v1690571969/pinktat_cogjsh.mp4",
+  },
+];
