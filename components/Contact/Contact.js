122dae51b1fd1437329be516ea259be4e33702d1
Aryse Tansy <medictansy@gmail.com>
2023-08-06 12:59:19 -0400

    Minor pad adjust

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -2,10 +2,12 @@ import ContactForm from "./ContactForm/ContactForm";
 import Map from "./Map/Map";
 import Phone from "./Phone/Phone";
 import Wrapper from "./Wrapper/Wrapper";
+import Hours from "./Hours/Hours";
 
 const Contact = () => {
   return (
     <Wrapper>
+      <Hours />
       <ContactForm />
       <Phone />
       <Map />
--- components/Contact/Hours/Hours.jsx
+++ components/Contact/Hours/Hours.jsx
@@ -1,8 +1,8 @@
 const Hours = () => {
   return (
     <ul className="px-5 list-none flex gap-5 flex-wrap justify-between items-center font-marker   bg-slate-50 border-solid border-b-4  border-[#0d1322]  w-full text-end   text-[#0d1322] text-[1em] sm:text-xl  md:text-5xl xl:text-5xl  min-h-[18vh] sm:min-h-[25vh] ">
-      <li className="   text-rose-600">Sun</li>
-      <li className=" text-rose-600">Mon</li>
+      <li className=" line-through  text-rose-600">Sun</li>
+      <li className="line-through  text-rose-600">Mon</li>
       <li>Tue</li>
       <li>Wed</li>
       <li>Thu</li>
